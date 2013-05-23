3fee7900f9726d1c3238973885eec8c1426c826d
hit9 <nz2324@126.com>
2013-05-23 18:58:58 +0800

    Add color utility

--- lilac/utils.py
+++ lilac/utils.py
@@ -39,3 +39,42 @@ def update_nested_dict(a, b):
         else:
             a[k] = v
     return a
+
+
+class Color(object):
+    """
+     utility to return ansi colored text
+     usage::
+
+         >>> from color import colored
+         >>> colored("text","red")
+         '\x1b[31mtext\x1b[0m'
+
+    """
+
+    colors = {
+        'black': 30,
+        'red': 31,
+        'green': 32,
+        'yellow': 33,
+        'blue': 34,
+        'magenta': 35,
+        'cyan': 36,
+        'white': 37,
+        'bgred': 41,
+        'bggrey': 100
+    }
+
+    prefix = '\033['
+
+    suffix = '\033[0m'
+
+    def colored(self, text, color=None):
+        if color not in self.colors:
+            color = 'white'
+
+        clr = self.colors[color]
+        return (self.prefix+'%dm%s'+self.suffix) % (clr, text)
+
+
+colored = Color().colored
